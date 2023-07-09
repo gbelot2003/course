4235b3d511376087ce37fa0906a5ee54bd622f28
Gerardo Belot <gbelot2003@hotmail.com>
2023-07-09 01:10:52 -0600

    🚧 core

--- www/bootstrap.php
+++ www/bootstrap.php
@@ -1,5 +1,5 @@
 <?php
 
-require_once './libraries/Core.php';
-require_once './libraries/Controller.php';
-require_once './libraries/Database.php';
+require_once 'libraries/Core.php';
+
+$init = new Core();
